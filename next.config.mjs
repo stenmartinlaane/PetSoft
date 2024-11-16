9c4f0f8a7977fa09aa4b77272414d691a4800cf4
stlaan <stlaan@ttu.ee>
2024-11-16 14:30:17 +0200

    Login page ui

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {protocol: "https", hostname: "bytegrad.com"}
+    ]
+  }
+};
 
 export default nextConfig;
